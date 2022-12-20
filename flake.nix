9f8503fc84fe22650847425d7b96001b074789c1
Jacob Chvatal <jake@isnt.online>
2022-12-20 00:50:20 -0500

    add nix

---
+++ flake.nix
@@ -0,0 +1,51 @@
+{
+  description = "Cross-platform window management and OS integration library for Java";
+
+  inputs = {
+    nixpkgs.url     = github:nixos/nixpkgs/release-22.05;
+    flake-utils.url = github:numtide/flake-utils;
+
+    # Used for shell.nix
+    flake-compat = {
+      url = github:edolstra/flake-compat;
+      flake = false;
+    };
+  };
+
+  outputs = {self, nixpkgs, flake-utils, ...} @ inputs:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {inherit system;};
+        xDependencies = with pkgs; [
+          xorg.xorgserver
+
+          # xorg input modules
+          xorg.xf86inputevdev
+          xorg.xf86inputsynaptics
+          xorg.xf86inputlibinput
+
+          # xorg video modules
+          xorg.xf86videointel
+          xorg.xf86videoati
+          xorg.xf86videonouveau
+        ];
+      in rec {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            jdk11
+          ] ++ xDependencies;
+
+          buildInputs = with pkgs; [
+            python3
+            libGL
+            ninja
+          ];
+
+          LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath (with pkgs; [ libGL ] ++ xDependencies)}:$LD_LIBRARY_PATH";
+        };
+
+        # For compatibility with older versions of the `nix` binary
+        devShell = self.devShells.${system}.default;
+      }
+    );
+}
